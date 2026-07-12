ef5512b154c136cc0531ff6cb3da91a6cb2df179
Yassir El bakkari <yel-bakk@c2r7p13.1337.ma>
2026-07-12 22:30:32 +0100

    save

--- src/ui.py
+++ src/ui.py
@@ -16,16 +16,32 @@ def visual():
     pygame.display.set_caption("Pacman | yel-bakk")
     score = 0
     power = 0
-    font1 = pygame.font.Font("../font/PAC-FONT.TTF", 150)
-    font2 = pygame.font.Font("../font/Gokart Bubble.otf", 20)
-    font3 = pygame.font.Font("../font/SoundWaveRacing-Regular.ttf", 70)
+    font1 = pygame.font.Font("font/PAC-FONT.TTF", 150)
+    font2 = pygame.font.Font("font/Gokart Bubble.otf", 20)
+    font3 = pygame.font.Font("font/SoundWaveRacing-Regular.ttf", 70)
     text1 = font1.render("PACMAN", True, (255, 255, 0))
     button_start = pygame.Rect(560, 390, 484, 55)
     button_view = pygame.Rect(455, 500, 697, 55)
     button_instr = pygame.Rect(530, 610, 558, 55)
     button_exit = pygame.Rect(725, 720, 179, 55)
-    pacman = pygame.image.load("../image/pacman.png")
-    pacman = pygame.transform.scale(pacman, (tile_size - 4, tile_size - 4))
+    pacman_1 = pygame.image.load("image/right_pacman.png")
+    pacman_1 = pygame.transform.scale(pacman_1, (tile_size - 4, tile_size - 4))
+    pacman_2 = pygame.image.load("image/down_pacman.png")
+    pacman_2 = pygame.transform.scale(pacman_2, (tile_size - 4, tile_size - 4))
+    pacman_3 = pygame.image.load("image/north_pacman.png")
+    pacman_3 = pygame.transform.scale(pacman_3, (tile_size - 4, tile_size - 4))
+    pacman_4 = pygame.image.load("image/above_pacman.png")
+    pacman_4 = pygame.transform.scale(pacman_4, (tile_size - 4, tile_size - 4))
+    ora_ghost = pygame.image.load("image/orange_ghost.png")
+    ora_ghost = pygame.transform.scale(ora_ghost, (tile_size - 4, tile_size - 4))
+    pink_ghost = pygame.image.load("image/pink_ghost.png")
+    pink_ghost = pygame.transform.scale(pink_ghost, (tile_size - 4, tile_size - 4))
+    red_ghost = pygame.image.load("image/red_ghost.png")
+    red_ghost = pygame.transform.scale(red_ghost, (tile_size - 4, tile_size - 4))
+    blue_ghost = pygame.image.load("image/blue_ghost.png")
+    blue_ghost = pygame.transform.scale(blue_ghost, (tile_size - 4, tile_size - 4))
+    
+    p = [True, False, False, False]
     pacman_col, pacman_row = end
     pacman_col = pacman_col // 2
     pacman_row = pacman_row // 2
@@ -48,9 +64,14 @@ def visual():
         if not (posx <= col < posx + len(ft_small[0])):
             return False
         return ft_small[row - posy][col - posx] == 1
-    power_pellets = [(1, 1), (len(maze[0]) - 2, 1),
-                     (1, len(maze) - 2),
-                     (len(maze[0]) - 2, len(maze) - 2)]
+    power_pellets = [(0, 0), (len(maze[0]) - 1, 0),
+                     (0, len(maze) - 1),
+                     (len(maze[0]) - 1, len(maze) - 1)]
+
+    # ghosts = [(1, 1),
+    #           (len(maze[0]) - 2, 1),
+    #           (1, len(maze) - 2),
+    #           (len(maze[0] - 2, len(maze) - 1))]
     while run:
         
         for event in pygame.event.get():
@@ -71,15 +92,19 @@ def visual():
                 cell = maze[pacman_row][pacman_col]
                 if event.key == pygame.K_UP and not (cell & 1):
                     pacman_row -= 1
+                    p = [False, False, False, True]
                     print("UP")
                 elif event.key == pygame.K_RIGHT and not (cell & 2):
                     pacman_col += 1
+                    p = [True, False, False, False]
                     print("RIGHT")
                 elif event.key == pygame.K_DOWN and not (cell & 4):
                     pacman_row += 1
+                    p = [False, True, False, False]
                     print("DOWN")
                 elif event.key == pygame.K_LEFT and not (cell & 8):
                     pacman_col -= 1
+                    p = [False, False, True, False]
                     print("LEFT")
         mouse_pos = pygame.mouse.get_pos()
         text2 = font3.render("Start Game", True, (0, 255, 0))
@@ -118,14 +143,26 @@ def visual():
 
 
 
+
             for row in range(len(maze)):
                 for col in range(len(maze[row])):
                     if pacgums[row][col] and not is_42(row, col):
                         pygame.draw.circle(
                             screen, (255, 255, 255),
                             (col * tile_size + tile_size // 2, row * tile_size + tile_size // 2), 3)
-                    
-            screen.blit(pacman, (pacman_col * tile_size + 2, pacman_row * tile_size + 2))
+
+            if p[0]:
+                screen.blit(pacman_1, (pacman_col * tile_size + 2, pacman_row * tile_size + 2))
+            if p[1]:
+                screen.blit(pacman_2, (pacman_col * tile_size + 2, pacman_row * tile_size + 2))
+            if p[2]:
+                screen.blit(pacman_3, (pacman_col * tile_size + 2, pacman_row * tile_size + 2))
+            if p[3]:
+                screen.blit(pacman_4, (pacman_col * tile_size + 2, pacman_row * tile_size + 2))
+            screen.blit(blue_ghost, (1 * tile_size, 1 * tile_size))
+            screen.blit(ora_ghost, ((len(maze[0]) - 2) * tile_size, 1 * tile_size))
+            screen.blit(pink_ghost, (1 * tile_size, (len(maze) - 2) * tile_size))
+            screen.blit(red_ghost, ((len(maze[0]) - 2) * tile_size, (len(maze) - 2) * tile_size))
             if pacgums[pacman_row][pacman_col]:
                 pacgums[pacman_row][pacman_col] = False
                 score += 1
